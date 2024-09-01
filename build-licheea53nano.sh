89b19f7e9d77e5994bd3b136fc13f67e87ec4c20
scpcom <scpcom@gmx.de>
2024-09-01 13:56:46 +0000

    add build scripts based on github workflow

---
+++ build-licheea53nano.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+if [ -e prepare-licheesgnano.sh ]; then
+  bash -e prepare-licheesgnano.sh
+fi
+
+source build/cvisetup.sh
+defconfig sg2002_licheea53nano_sd
+build_all
+# build other variant
+cp -p build/boards/sg200x/sg2002_licheea53nano_sd/sg2002_licheea53nano_sd_defconfig bak.config
+
+# 2.8inch
+cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_HD228001C31/g' > build/boards/sg200x/sg2002_licheea53nano_sd/sg2002_licheea53nano_sd_defconfig
+defconfig sg2002_licheea53nano_sd
+clean_uboot
+clean_opensbi
+clean_fsbl
+build_fsbl
+cp -v install/soc_sg2002_licheea53nano_sd/fip.bin install/soc_sg2002_licheea53nano_sd/hd228001c31.bin
+
+# 3inch
+cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_D300FPC9307A/g' > build/boards/sg200x/sg2002_licheea53nano_sd/sg2002_licheea53nano_sd_defconfig
+defconfig sg2002_licheea53nano_sd
+clean_uboot
+clean_opensbi
+clean_fsbl
+build_fsbl
+cp -v install/soc_sg2002_licheea53nano_sd/fip.bin install/soc_sg2002_licheea53nano_sd/d300fpc9307a.bin
+
+# 5inch
+cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_DXQ5D0019B480854/g' > build/boards/sg200x/sg2002_licheea53nano_sd/sg2002_licheea53nano_sd_defconfig
+defconfig sg2002_licheea53nano_sd
+clean_uboot
+clean_opensbi
+clean_fsbl
+build_fsbl
+cp -v install/soc_sg2002_licheea53nano_sd/fip.bin install/soc_sg2002_licheea53nano_sd/dxq5d0019b480854.bin
+
+mv bak.config build/boards/sg200x/sg2002_licheea53nano_sd/sg2002_licheea53nano_sd_defconfig
+
+echo OK
