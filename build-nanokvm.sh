edb25788bd6b8cae9e1d703470a0b6cdfc763c2b
scpcom <scpcom@gmx.de>
2024-09-01 13:57:23 +0000

    add build script for nanokvm

---
+++ build-nanokvm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+cd build
+# enable usb disk, disable ncm
+sed -i s/'usb.ncm'/'usb.disk0'/g tools/common/sd_tools/genimage_rootless.cfg
+sed -i 's|touch ${output_dir}/input/usb.ncm|echo /dev/mmcblk0p3 > ${output_dir}/input/usb.disk0|g' tools/common/sd_tools/sd_gen_burn_image_rootless.sh
+# enable usb hid
+if ! grep -q "usb.hid" tools/common/sd_tools/genimage_rootless.cfg ; then
+  sed -i s/'\t\t\t"usb.disk0",'/'\t\t\t"usb.disk0",\n\t\t\t"usb.hid",'/g tools/common/sd_tools/genimage_rootless.cfg
+fi
+if ! grep -q "usb.hid" tools/common/sd_tools/sd_gen_burn_image_rootless.sh ; then
+  sed -i 's| \${output_dir}/input/usb.disk0$| ${output_dir}/input/usb.disk0\ntouch ${output_dir}/input/usb.hid|g' tools/common/sd_tools/sd_gen_burn_image_rootless.sh
+fi
+cd ..
+
+cd buildroot
+# enable nanokvm app, disable tpudemo
+sed -i s/'BR2_PACKAGE_TPUDEMO_SG200X=y'/'BR2_PACKAGE_NANOKVM_SG200X=y'/g configs/cvitek_SG200X_musl_riscv64_defconfig
+cd ..
+
+source build/cvisetup.sh
+defconfig sg2002_licheervnano_sd
+build_all
+
+cd build
+git restore tools/common/sd_tools/genimage_rootless.cfg
+git restore tools/common/sd_tools/sd_gen_burn_image_rootless.sh
+cd ..
+
+cd buildroot
+git restore configs/cvitek_SG200X_musl_riscv64_defconfig
+cd ..
+
+echo OK
