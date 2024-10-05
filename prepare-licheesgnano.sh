90b057ef422c8ba5b5833fd6ae71701920c7b0e8
scpcom <scpcom@gmx.de>
2024-10-05 20:38:24 +0200

    add missing symlinks before build

--- prepare-licheesgnano.sh
+++ prepare-licheesgnano.sh
@@ -3,17 +3,29 @@
 if git -C linux_5.10 checkout -b build 2>/dev/null ; then
   ln -sf ../../../../build/output/sg2002_licheervnano_sd/cvi_board_memmap.h linux_5.10/scripts/dtc/include-prefixes/cvi_board_memmap.h
   git -C linux_5.10 add scripts/dtc/include-prefixes/cvi_board_memmap.h
-  d=`pwd`/build/boards/sg200x/sg2002_licheervnano_sd/dts_riscv
-  for f in $d/*.dts ; do
-    b=`basename $f`
-    ln -sf $d/$b linux_5.10/arch/riscv/boot/dts/cvitek/$b
-    git -C linux_5.10 add arch/riscv/boot/dts/cvitek/$b
+  for a in sg200x/sg2000_duo_sd sg200x/sg2002_duo_sd sg200x/sg2002_licheervnano_sd ; do
+    d=`pwd`/build/boards/$a/dts_riscv
+    for f in $d/*.dts ; do
+      b=`basename $f`
+      ln -sf $d/$b linux_5.10/arch/riscv/boot/dts/cvitek/$b
+      git -C linux_5.10 add arch/riscv/boot/dts/cvitek/$b
+    done
+  done
+  for a in sg200x/sg2002_licheea53nano_sd sg200x/sg2000_wevb_sd ; do
+    d=`pwd`/build/boards/$a/dts_arm
+    for f in $d/*.dts ; do
+      b=`basename $f`
+      ln -sf $d/$b linux_5.10/arch/arm/boot/dts/cvitek/$b
+      git -C linux_5.10 add arch/arm/boot/dts/cvitek/$b
+    done
   done
   d=`pwd`/build/boards/default/dts/sg200x
   for f in $d/*.dtsi ; do
     b=`basename $f`
     ln -sf $d/$b linux_5.10/arch/riscv/boot/dts/cvitek/$b
     git -C linux_5.10 add arch/riscv/boot/dts/cvitek/$b
+    ln -sf $d/$b linux_5.10/arch/arm/boot/dts/cvitek/$b
+    git -C linux_5.10 add arch/arm/boot/dts/cvitek/$b
   done
   git -C linux_5.10 commit -m "licheervnano: symlink dts and memmap"
 fi
