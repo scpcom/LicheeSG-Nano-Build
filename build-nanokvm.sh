336e2c8579c994a69e9e86ce843bba8371b06fb7
scpcom <scpcom@gmx.de>
2024-10-10 04:42:02 +0200

    nanokvm: do not sync kvm and tailscale scripts to overlay/etc/init.d on second run

--- build-nanokvm.sh
+++ build-nanokvm.sh
@@ -45,6 +45,8 @@ cd ..
 cd buildroot
 if [ -e output/per-package/nanokvm-sg200x/target/kvmapp/system/init.d ]; then
   rsync -r --verbose --copy-dirlinks --copy-links --hard-links output/per-package/nanokvm-sg200x/target/kvmapp/system/init.d/ board/cvitek/SG200X/overlay/etc/init.d/
+  rm -f board/cvitek/SG200X/overlay/etc/init.d/S*kvm*
+  rm -f board/cvitek/SG200X/overlay/etc/init.d/S*tailscale*
 fi
 # enable nanokvm app, disable tpudemo
 sed -i s/'^BR2_PACKAGE_TPUDEMO_SG200X=y'/'BR2_PACKAGE_MAIX_CDK=y\nBR2_PACKAGE_NANOKVM_SG200X=y'/g configs/cvitek_SG200X_musl_riscv64_defconfig
