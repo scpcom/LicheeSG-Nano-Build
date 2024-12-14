2d6280e0f0c28e7e2e8384e31f16451a7182697e
scpcom <scpcom@gmx.de>
2024-12-14 03:00:05 +0100

    build tpu sdk if submodules are available

--- build-licheervnano.sh
+++ build-licheervnano.sh
@@ -14,6 +14,26 @@ fi
 
 source build/cvisetup.sh
 defconfig sg2002_licheervnano_sd
+
+# small fixes to avoid forks of tpu related repositories at this moment
+if [ -e cvi_rtsp ]; then
+  sed -i s/'${RANLIB} LIVE555_DIR='/'${RANLIB} CHIP_ARCH=${CHIP_ARCH} LIVE555_DIR='/g cvi_rtsp/build.sh
+  if ! grep -q "D__CV181X__" cvi_rtsp/Makefile.inc ; then
+    sed -i 's/CFLAGS += -DARCH_$(CHIP_ARCH)/CFLAGS += -DARCH_$(CHIP_ARCH)\n\nifeq ("$(CHIP_ARCH)", "SG200X")\nCFLAGS += -D__CV181X__\nendif\nifeq ("$(CHIP_ARCH)", "CV181X")\nCFLAGS += -D__CV181X__\nendif\nifeq ("$(CHIP_ARCH)", "CV180X")\nCFLAGS += -D__CV180X__\nendif/g' cvi_rtsp/Makefile.inc
+  fi
+  sed -i 's|-I$(KERNEL_INC)$|-I$(KERNEL_INC) -I$(MW_DIR)/component/isp/common -I$(MW_DIR)/../osdrv/interdrv/include/common/uapi/linux|g' cvi_rtsp/Makefile.inc
+  sed -i 's|-I$(MW_DIR)/sample/common -I$(MW_DIR)/include/isp|-I$(MW_DIR)/sample/common -I$(MW_DIR)/component/isp/common -I$(MW_DIR)/include/isp|g' cvi_rtsp/Makefile.inc
+fi
+if [ -e cviruntime -a -e flatbuffers ]; then
+  sed -i s/'CHIP_ID="${CHIP_ARCH,,}"'/'CHIP_ID="${CHIP_ARCH,,}"\n[ $CHIP_ID = sg200x ] \&\& CHIP_ID=cv181x'/g cviruntime/build_tpu_sdk.sh
+  sed -i s/'-Werror=unused-parameter"'/'-Werror=unused-parameter -Wno-class-memaccess"'/g flatbuffers/CMakeLists.txt
+  export TPU_REL=1
+fi
+if [ -e tdl_sdk ]; then
+  sed -i s/'${CROSS_COMPILE} SDK_VER='/'${CROSS_COMPILE} LIVE555_DIR=${MLIR_SDK_ROOT} SDK_VER='/g tdl_sdk/cmake/cvi_rtsp.cmake
+  sed -i s/'elif .. "$CHIP_ARCH" == "CV181X" ..; then'/'elif [[ "$CHIP_ARCH" == "CV181X" ]] || [[ "$CHIP_ARCH" == "SG200X" ]]; then\n    CHIP_ARCH=CV181X'/g tdl_sdk/scripts/*sdk_release.sh
+fi
+
 build_all
 # build other variant
 cp -p build/boards/sg200x/sg2002_licheervnano_sd/sg2002_licheervnano_sd_defconfig bak.config
