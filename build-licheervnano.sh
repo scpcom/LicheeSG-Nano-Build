d1d6669eb1e5c660498f6b773adaf4a9a26e8135
scpcom <scpcom@gmx.de>
2024-12-22 22:06:07 +0100

    unify build scripts

--- build-licheervnano.sh
+++ build-licheervnano.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+export SG_BOARD_FAMILY=sg200x
+export SG_BOARD_LINK=sg2002_licheervnano_sd
+
 for p in / /usr/ /usr/local/ ; do
   if echo $PATH | grep -q ${p}bin ; then
     if ! echo $PATH | grep -q ${p}sbin ; then
@@ -13,7 +16,7 @@ if [ -e prepare-licheesgnano.sh ]; then
 fi
 
 source build/cvisetup.sh
-defconfig sg2002_licheervnano_sd
+defconfig ${SG_BOARD_LINK}
 
 if [ -e cviruntime -a -e flatbuffers ]; then
   # small fix to keep fork of flatbuffers repository optional
@@ -22,36 +25,37 @@ if [ -e cviruntime -a -e flatbuffers ]; then
 fi
 
 build_all
+
 # build other variant
-cp -p build/boards/sg200x/sg2002_licheervnano_sd/sg2002_licheervnano_sd_defconfig bak.config
+cp -p build/boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig bak.config
 
 # 2.8inch
-cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_HD228001C31/g' > build/boards/sg200x/sg2002_licheervnano_sd/sg2002_licheervnano_sd_defconfig
-defconfig sg2002_licheervnano_sd
+cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_HD228001C31/g' > build/boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
+defconfig ${SG_BOARD_LINK}
 clean_uboot
 clean_opensbi
 clean_fsbl
 build_fsbl
-cp -v install/soc_sg2002_licheervnano_sd/fip.bin install/soc_sg2002_licheervnano_sd/hd228001c31.bin
+cp -v install/soc_${SG_BOARD_LINK}/fip.bin install/soc_${SG_BOARD_LINK}/hd228001c31.bin
 
 # 3inch
-cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_D300FPC9307A/g' > build/boards/sg200x/sg2002_licheervnano_sd/sg2002_licheervnano_sd_defconfig
-defconfig sg2002_licheervnano_sd
+cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_D300FPC9307A/g' > build/boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
+defconfig ${SG_BOARD_LINK}
 clean_uboot
 clean_opensbi
 clean_fsbl
 build_fsbl
-cp -v install/soc_sg2002_licheervnano_sd/fip.bin install/soc_sg2002_licheervnano_sd/d300fpc9307a.bin
+cp -v install/soc_${SG_BOARD_LINK}/fip.bin install/soc_${SG_BOARD_LINK}/d300fpc9307a.bin
 
 # 5inch
-cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_DXQ5D0019B480854/g' > build/boards/sg200x/sg2002_licheervnano_sd/sg2002_licheervnano_sd_defconfig
-defconfig sg2002_licheervnano_sd
+cat bak.config | sed -e 's/CONFIG_MIPI_PANEL_ZCT2133V1/CONFIG_MIPI_PANEL_ST7701_DXQ5D0019B480854/g' > build/boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
+defconfig ${SG_BOARD_LINK}
 clean_uboot
 clean_opensbi
 clean_fsbl
 build_fsbl
-cp -v install/soc_sg2002_licheervnano_sd/fip.bin install/soc_sg2002_licheervnano_sd/dxq5d0019b480854.bin
+cp -v install/soc_${SG_BOARD_LINK}/fip.bin install/soc_${SG_BOARD_LINK}/dxq5d0019b480854.bin
 
-mv bak.config build/boards/sg200x/sg2002_licheervnano_sd/sg2002_licheervnano_sd_defconfig
+mv bak.config build/boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
 
 echo OK
