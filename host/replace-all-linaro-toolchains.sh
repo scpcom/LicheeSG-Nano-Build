4e977185a44bc093dde592e4e7481b9f2204e8b5
scpcom <scpcom@gmx.de>
2024-12-27 21:48:49 +0100

    host: add replace linaro sysroots to script

--- host/replace-all-linaro-toolchains.sh
+++ host/replace-all-linaro-toolchains.sh
@@ -22,8 +22,13 @@ for gctgt in $gctgts ; do
   if [ ! -e ${gctar} ]; then
     wget -N ${tcurl}/${tcver}-${tcdat}/${gctgt}/${gctar}
   fi
-  #gctar=runtime-gcc-linaro-${gcver}-${tcdat}-${gctgt}.tar.xz
-  #gctar=sysroot-glibc-linaro-${lcver}-${tcdat}-${gctgt}.tar.xz
+  #rttar=runtime-gcc-linaro-${gcver}-${tcdat}-${gctgt}.tar.xz
+  if echo ${gctgt} | grep -q linux ; then
+    srtar=sysroot-glibc-linaro-${lcver}-${tcdat}-${gctgt}.tar.xz
+    if [ ! -e ${srtar} ]; then
+      wget -N ${tcurl}/${tcver}-${tcdat}/${gctgt}/${srtar}
+    fi
+  fi
 done
 cd ..
 
@@ -36,6 +41,20 @@ cd gcc
 for gctgt in $gctgts ; do
   gctar=gcc-linaro-${gcver}-${tcdat}-${harch}_${gctgt}.tar.xz
   tar xJf ${d}/${gctar}
+done
+cd ../..
+
+mkdir -p ramdisk
+
+cd ramdisk/
+rm -rf sysroot/sysroot-*linaro-*
+mkdir -p sysroot
+cd sysroot
+for gctgt in $gctgts ; do
+  if echo ${gctgt} | grep -q linux ; then
+    srtar=sysroot-glibc-linaro-${lcver}-${tcdat}-${gctgt}.tar.xz
+    tar xJf ${d}/${srtar}
+  fi
 done
 cd ../..
 
