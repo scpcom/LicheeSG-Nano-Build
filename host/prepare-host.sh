d0c2e8c3557d1db785a9fb26622421ffb075fcd6
scpcom <scpcom@gmx.de>
2025-02-01 21:32:38 +0100

    host: prepare: install liblzma-dev

--- host/prepare-host.sh
+++ host/prepare-host.sh
@@ -7,7 +7,7 @@ installpkgs(){
   echo "Installing required packages..."
   apt-get install -y cpio xxd
   apt-get install -y build-essential cmake git pkg-config rsync unzip wget zip
-  apt-get install -y bc bison flex libncurses-dev libssl-dev device-tree-compiler
+  apt-get install -y bc bison flex liblzma-dev libncurses-dev libssl-dev device-tree-compiler
   apt-get install -y ninja-build
   apt-get install -y dosfstools file mtools
   apt-get install -y fuse2fs shellcheck
