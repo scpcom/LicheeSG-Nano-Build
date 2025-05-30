0539e7f2bc66a168bcac1644b7454e3d3269f663
scpcom <scpcom@gmx.de>
2025-05-31 01:47:43 +0200

    nanokvm: move sdk ver switching to prepare

--- build-nanokvm.sh
+++ build-nanokvm.sh
@@ -91,54 +91,10 @@ if echo ${SG_BOARD_LINK} | grep -q -E '^sg200' ; then
 fi
 
 if [ -e prepare-licheesgnano.sh ]; then
-  bash -e prepare-licheesgnano.sh
+  . ./prepare-licheesgnano.sh
 fi
-
-sdkcros=linux-gnu
-sdklibc=`echo $sdkver | cut -d '_' -f 1`
-sdkarch=`echo $sdkver | cut -d '_' -f 2`
-sdktool=`echo $sdkver | tr a-z A-Z`
-oldcros=$sdkcros
-oldlibc=$sdklibc
-oldarch=$sdkarch
-# Allow to switch from ARM 32-bit to 64-bit and vice versa
-if [ $sdkver = glibc_arm64 ]; then
-  oldarch=arm
-elif [ $sdkver = glibc_arm ]; then
-  oldarch=arm64
-fi
-# Allow to switch from RISC-V musl to glibc and vice versa
-if [ $sdkver = musl_riscv64 ]; then
-  sdkcros=linux-musl
-  oldlibc=glibc
-elif [ $sdkver = glibc_riscv64 ]; then
-  oldcros=linux-musl
-  oldlibc=musl
-fi
-oldtool=`echo ${oldlibc}_${oldarch} | tr a-z A-Z`
-[ $oldarch = riscv64 ] && oldarch=riscv
-[ $sdkarch = riscv64 ] && sdkarch=riscv
 
 cd build
-if [ $sdkcros != $oldcros ]; then
-  sed -i s/'-unknown-'${oldcros}'-'/'-unknown-'${sdkcros}'-'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
-fi
-if [ $sdktool != $oldtool -a $sdkver != keep ]; then
-  if ! grep -q -E '^CONFIG_TOOLCHAIN_.*=y' boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig ; then
-    echo 'CONFIG_TOOLCHAIN_'${sdktool}'=y' >> boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
-  else
-    sed -i s/'^CONFIG_TOOLCHAIN_'${oldtool}'=y'/'CONFIG_TOOLCHAIN_'${sdktool}'=y'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
-  fi
-fi
-if [ $sdkarch != $oldarch ]; then
-  if ! grep -q -E '^CONFIG_ARCH=".*"' boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig ; then
-    echo 'CONFIG_ARCH="'${sdkarch}'"' >> boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
-  else
-    sed -i s/'^CONFIG_ARCH="'${oldarch}'"'/'CONFIG_ARCH="'${sdkarch}'"'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/${SG_BOARD_LINK}_defconfig
-  fi
-  [ -e boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/dts_${oldarch} -a \
-  ! -e boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/dts_${sdkarch} ] && ln -s dts_${oldarch} boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/dts_${sdkarch}
-fi
 # Expand user space RAM from 128MB to 160MB
 sed -i s/'ION_SIZE = .* . SIZE_1M'/'ION_SIZE = 75 * SIZE_1M'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/memmap.py
 sed -i s/'BOOTLOGO_SIZE = .* . SIZE_1K'/'BOOTLOGO_SIZE = 5632 * SIZE_1K'/g boards/${SG_BOARD_FAMILY}/${SG_BOARD_LINK}/memmap.py
