b6fc932e1fd7aabc96daac630f240e38d8595386
scpcom <scpcom@gmx.de>
2025-10-25 00:07:54 +0200

    nanokvm: disable qt5

--- build-nanokvm.sh
+++ build-nanokvm.sh
@@ -249,6 +249,7 @@ if [ $shrink = y ]; then
   sed -i /'BR2_PACKAGE_FFMPEG'/d configs/${BR_DEFCONFIG}
   sed -i /'BR2_PACKAGE_MPG123'/d configs/${BR_DEFCONFIG}
   sed -i /'BR2_PACKAGE_OPENCV'/d configs/${BR_DEFCONFIG}
+  sed -i /'BR2_PACKAGE_QT5'/d configs/${BR_DEFCONFIG}
 fi
 if [ $maixcdk = n -a $shrink = y ]; then
   sed -i /'^BR2_PACKAGE_FFMPEG'/d configs/${BR_DEFCONFIG}
